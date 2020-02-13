e4a8dc70d50c7c0c4168241d1d38cc396ee8aabe
Kiran <kiru741@gmail.com>
2020-02-13 22:09:46 +0530

    character component added

---
+++ src/components/card/characterContainer.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Container = styled.div`
+    width: 300px;
+    background-color: #093009;
+    border: 1px solid black;
+    margin-left: 1px;
+    margin-right: 3px;
+    margin-bottom: 5px;
+`;
+
+const CharacterImg = styled.img`
+    width: 300px;
+`;
+
+const CharacterName = styled.div `
+    padding: 20px;
+    color: white;
+    font-size: 18px;
+    text-align: center;
+`;
+
+const CharacterSmallText = styled.div `
+    color: white;
+    font-size: 16px;
+    padding: 10px;
+    text-align: left;
+`;
+
+const CharacterContainer = (props) => {
+    return (
+        <Container>
+            <div>
+                <CharacterImg src="https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_mike-ehrmantraut-lg.jpg"/>
+                <CharacterName>{props.name}</CharacterName>
+                <CharacterSmallText> Nickname:  {props.nickName}</CharacterSmallText>
+                <CharacterSmallText> Occupation: {props.occupation} </CharacterSmallText>
+            </div>
+        </Container>
+    );
+}
+
+export default CharacterContainer;
